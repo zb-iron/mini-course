07412762f1697946625224db372bc03b55ddaea3
cslee <cslee@fondee.io>
2023-10-16 20:33:38 +0900

    first commit

---
+++ src/main/java/com/minicourse/iron/zb/minicourse/MiniCourseApplication.java
@@ -0,0 +1,13 @@
+package com.minicourse.iron.zb.minicourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minicourse/iron/zb/minicourse/MiniCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minicourse.iron.zb.minicourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
